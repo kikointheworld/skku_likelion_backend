10137fe634f5a57c19bcd8dba4c52d3c12fca8d0
kkkiko <popo9855@g.skku.edu>
2022-04-22 17:03:05 +0900

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HTTPResponse
+from django.shortcuts import render
+
+def index(request):
+    return HTTPResponse("Hello, Django!")
